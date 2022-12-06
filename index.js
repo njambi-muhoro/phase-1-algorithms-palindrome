2a4345cce3a54770e7afbe093e56233422fa6264
+ <+>
2022-12-06 16:55:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  // covert our string to an array and then reverse then join to get the reverse of out string
+  let newArray = word.split('');
+  //console.log(newArray)
+
+  let newReversedArray = newArray.reverse();
+ // console.log(newReversedArray)
+
+ let joinReverseArray = newReversedArray.join('')
+ console.log(joinReverseArray);
+ if(word === joinReverseArray){
+   console.log("Is palindrome")
+ }
+ else{
+   console.log("Is not palindrome")
+ }
 }
+ isPalindrome("racecar")
+  
+
 
 /* 
   Add your pseudocode here
+  1. turn the string into an array by using split('') method
+  2. reverse the new array using the reverse method
+  3. join the revered array using the join('') method
+  4. do a comparison of the initial string to the new string f
+
 */
 
-/*
-  Add written explanation of your solution here
+/* /*
+  I expect (racecar) == (racecar) or helloworld !== dlrowolloeh
+  (racecar) == (racecar) will console.log("Is palindrome")
+  (helloworld !== dlrowolloeh) will console.log("Is not a palindrome")
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
+  console.log("Expecting:racecar=== racecar to equal true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("is palindrome");
 
-  console.log("Expecting: false");
+  console.log("Expecting: helloworld === dlrowolloeh false");
   console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+}module.exports = isPalindrome; 
